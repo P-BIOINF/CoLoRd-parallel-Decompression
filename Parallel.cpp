3c04363f7cce238b9fd524c28b49907b87e878a4
Aleksander Zak <olekzak7@gmail.com>
2024-03-12 15:58:20 +0100

    Porzucamy pomysl z API zrobimy za pomoca std::system

---
+++ Parallel.cpp
@@ -0,0 +1,8 @@
+﻿#include "Parallel.h"
+#include <execution>
+#include "Timer.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <unordered_set>
---
+++ Parallel.h
@@ -0,0 +1,13 @@
+﻿#ifndef PARALLEL_H
+#define PARALLEL_H
+
+#include <utility>
+#include <string>
+#include <vector>
+#include <filesystem>
+class Parallel
+{
+	;
+};
+
+#endif
